6044a626a3eb33b634455c7d64a993de81f801b0
Visu <visuttaja@gmail.com>
2022-04-06 03:23:48 +0300

    second time

---
+++ DropOnMe/DropOnMe/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace DropOnMe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(768, 145);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Log screen 2";
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AllowDrop = true;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 207);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(768, 95);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "https://www.youtube.com/watch?v=QRmmISj6Rrw&list=PLfdtiltiRHWFD41D_LDomY1Fb-O9MtF" +
+    "qq";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(317, 78);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load Playlist";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(802, 518);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DropOnMe/DropOnMe/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace DropOnMe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+                                                                               
+
+        private void textBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+           
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AlustaDropCallbackit(sender,  e);
+
+        }
+        private void AlustaDropCallbackit(object sender, EventArgs e)
+        {
+            textBox2.DragEnter += new DragEventHandler(miunDragEnterHandler);
+         //   textBox2.MouseDown += new MouseEventHandler(textBox1_MouseDown);
+            textBox2.DragDrop += new DragEventHandler(miunDropHandler);
+
+           
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.SelectAll();
+            tb.DoDragDrop(tb.Text, DragDropEffects.Copy);
+        }
+        //*************************************
+        private void miunDropHandler(object sender, DragEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = (string)e.Data.GetData(DataFormats.Text);
+            tb.SelectAll();
+
+        }
+        //*************************************
+        private void miunDragEnterHandler(object sender, DragEventArgs e)
+        {
+            Type tyyppi1 = sender.GetType();
+            string [] formats = e.Data.GetFormats();
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Copy;
+          }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string urlSoittolista = "\""+textBox2.Text+"\"";
+            System.Environment.CurrentDirectory = @".\";
+            //System.Diagnostics.Process.Start("run1_setenv.bat", urlSoittolista);
+            //urlSoittolista = urlSoittolista.Replace("&", "^&");
+            ExecuteCommand("setenv_und_run.bat "+ urlSoittolista);
+
+        }
+       private void ExecuteCommand(string command)
+        {
+        
+            var processInfo = new ProcessStartInfo("cmd.exe", "/k " + command);
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            
+            //processInfo.RedirectStandardError = true;
+            //processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+            Console.WriteLine("qwerqwrq");
+            /*
+            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+                Console.WriteLine("output>>" + e.Data);
+            process.BeginOutputReadLine();
+
+
+            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
+                Console.WriteLine("error>>" + e.Data);
+            process.BeginErrorReadLine();
+            */
+            //process.WaitForExit();
+
+           // Console.WriteLine("ExitCode: {0}", process.ExitCode);
+           // process.Close();
+        }
+    }
+}
